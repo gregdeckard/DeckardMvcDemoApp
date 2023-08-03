bf1ac965b27201df7aef6d7236a4b03d52ccd462
deckag <44596498+gregdeckard@users.noreply.github.com>
2023-08-03 12:50:49 -0400

    Add project files.

---
+++ DeckardMvcDemoApp/Controllers/HumanResourcesController.cs
@@ -0,0 +1,32 @@
+﻿//using DeckardMvcDemoApp.DataAccess;
+using DeckardMvcDemoApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace DeckardMvcDemoApp.Controllers
+{
+    public class HumanResourcesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Building()
+        {
+            return View();
+        }
+
+        public IActionResult Employee()
+        {
+            return View();
+        }
+
+        public IActionResult Office()
+        {
+            return View();
+        }
+    }
+}
---
+++ DeckardMvcDemoApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeckardMvcDemoApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
